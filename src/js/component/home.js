05ff66de08dca140b7f0c768d792d074dffff3a8
Mykega <m.garcia13_3@hotmail.com>
2021-03-14 16:40:38 +0000

    primer compromiso

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,42 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export default class TrafficLights extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			clickedLight: null
+		};
+	}
 
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	render() {
+		console.log(this.state);
+		let redExtraClass = "";
+		if (this.state.clickedLight == "red") redExtraClass = "selected";
+		let yellowExtraClass = "";
+		if (this.state.clickedLight == "yellow") yellowExtraClass = "selected";
+		let greenExtraClass = "";
+		if (this.state.clickedLight == "green") greenExtraClass = "selected";
+		return (
+			<div>
+				<div id="trafficTop"></div>
+				<div id="container">
+					<div
+						className={"red light " + redExtraClass}
+						onClick={() =>
+							this.setState({ clickedLight: "red" })
+						}></div>
+					<div
+						className={"yellow light " + yellowExtraClass}
+						onClick={() =>
+							this.setState({ clickedLight: "yellow" })
+						}></div>
+					<div
+						className={"green light " + greenExtraClass}
+						onClick={() =>
+							this.setState({ clickedLight: "green" })
+						}></div>
+				</div>
+			</div>
+		);
+	}
 }
